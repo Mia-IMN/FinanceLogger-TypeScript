6b91e40e415cf9151d0a68d6890ada2ac3ffb6d1
Mia-IMN <mira.ikechukwu15@gmail.com>
2022-02-10 23:18:23 +0100

    first commit

---
+++ src/classes/Payment.ts
@@ -0,0 +1,23 @@
+import { HasFormatter } from "../interface/hasFormatter";
+ 
+export class Payment implements HasFormatter{
+    // recipient: string;
+    // details: string;
+    // amount: number;
+  
+    // constructor(c: string, d: string, a: number) {
+    //   this.recipient = c;
+    //   this.details = d;
+    //   this.amount = a;
+    // }
+  
+    constructor (
+        readonly recipient: string,
+        private details: string,
+        public amount: number, 
+    ){}
+    format() {
+      return `${this.recipient} is owed $${this.amount} for ${this.details}`;
+    }
+  }
+  
